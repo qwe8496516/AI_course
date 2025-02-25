bf14d23cd1ad8d658036135b01417ca1d56ab782
t113598012 <t113598012@ntut.edu.tw>
2025-02-25 16:41:52 +0800

    update: solution presentation

--- AI_code/aco.py
+++ AI_code/aco.py
@@ -2,35 +2,32 @@ import random
 import coordinate
 
 def ant_colony_optimization(alpha, beta, evaporation_rate, quality_factor, iterations, start_city, coordinates):
-    total_times = 0
-    distance_cost = coordinate.transfer_coordinate_to_cost_matrix(coordinates)
-    num_ants = len(distance_cost)
-    num_cities = len(distance_cost)
-    pheromone_matrix = [[0 if i == j else 1 for j in range(num_cities)] for i in range(num_cities)]
-    iteration_best_cost = []
-    best_path = []
-    min_cost = float('inf')
+    distance_cost = coordinate.transfer_coordinate_to_cost_matrix(coordinates)  # 座標轉換成距離矩陣
+    num_ants = len(distance_cost)   # 螞蟻數量
+    num_cities = len(distance_cost)   # 城市數量
+    pheromone_matrix = [[0 if i == j else 1 for j in range(num_cities)] for i in range(num_cities)]   # 初始費洛蒙濃度矩陣
+    iteration_best_cost = []   # 每次iteration的最短路徑長度
+    best_path = []   # 最短路徑
+    min_cost = float('inf')   # 最短路徑長度
 
     for i in range(iterations):
-        ant_paths = []
-        ant_distances = []
+        ant_paths = []  # 每次iteration所有螞蟻的路徑
+        ant_distances = []   # 螞蟻路徑長度
 
         for ant_No in range(num_ants):
-            init_city = ant_No
-            ant_path = [init_city]
-            ant_distance = 0
-            current_city = init_city
-            unvisited_cities = [city for city in range(num_cities)]
-            unvisited_cities.remove(init_city)
-
+            init_city = ant_No   # 起始城市 (分配到不同初始城市)
+            ant_path = [init_city]  # 加入初始城市到路徑
+            ant_distance = 0    
+            current_city = init_city  # 初始化當前城市
+            unvisited_cities = [city for city in range(num_cities)]  # 建立所有城市的列表
+            unvisited_cities.remove(init_city)  # 移除起點
+            
             while unvisited_cities:
                 probabilities = []
+                # 計算未到達城市的機率
                 for city in unvisited_cities:
-                    if distance_cost[current_city][city] == 0:
-                        continue
-                    
-                    distance = distance_cost[current_city][city]
-                    pheromone = pheromone_matrix[current_city][city]
+                    distance = distance_cost[current_city][city]    # 取得 current_city 與 city 之間的距離
+                    pheromone = pheromone_matrix[current_city][city]    # 計算 current_city 到 city 的費洛蒙濃度
                     probability = (pheromone ** alpha) * ((1 / distance) ** beta)
                     probabilities.append((city, probability))
 
@@ -40,16 +37,15 @@ def ant_colony_optimization(alpha, beta, evaporation_rate, quality_factor, itera
                     break
 
                 for city, probability in probabilities:
-                    probability = round(probability / total_probability, 2)
-                    # print(probability)
+                    probability = round(probability / total_probability, 2)  # 計算機率 
             
-                next_city = random.choices([city for city, _ in probabilities], [probability for _, probability in probabilities])[0]
-                ant_path.append(next_city)
-                ant_distance += distance_cost[current_city][next_city]
-                current_city = next_city
-                unvisited_cities.remove(next_city)
+                next_city = random.choices([city for city, _ in probabilities], [probability for _, probability in probabilities])[0]   # 隨機選擇下一個城市
+                ant_path.append(next_city)   # 將下一個 city 加入路徑
+                ant_distance += distance_cost[current_city][next_city]  # 取得 current_city 與 city 之間的距離
+                current_city = next_city    # 更新 current_city
+                unvisited_cities.remove(next_city)   # 移除已經走過的城市
 
-            ant_distance += distance_cost[next_city][init_city]
+            ant_distance += distance_cost[next_city][init_city]  # 回到起點
 
             if ant_distance < min_cost:
                 min_cost = ant_distance
@@ -58,6 +54,7 @@ def ant_colony_optimization(alpha, beta, evaporation_rate, quality_factor, itera
             ant_paths.append(ant_path)
             ant_distances.append(ant_distance)
 
+        # 更新費洛蒙濃度
         for i in range(num_cities):
             for j in range(num_cities):
                 pheromone_matrix[i][j] = pheromone_matrix[i][j] * (1 - evaporation_rate)
@@ -71,11 +68,11 @@ def ant_colony_optimization(alpha, beta, evaporation_rate, quality_factor, itera
         # print(ant_distances)
         iteration_best_cost.append(min(ant_distances))
 
-        if (is_all_same_element(ant_distances)):
-            total_times += 1
+        # if (is_all_same_element(ant_distances)):
+        #     total_times += 1
 
-        if total_times == 10:
-            break
+        # if total_times == 10:
+        #     break
     
     # print('best_path:', best_path)
     s_index = best_path.index(start_city)
--- AI_code/dp.py
+++ AI_code/dp.py
@@ -1,54 +1,52 @@
 import coordinate
 
 def dynamic_programming(start_city, coordinates):
-    distance_cost = coordinate.transfer_coordinate_to_cost_matrix(coordinates)
-    num_cities = len(distance_cost)
-    all_cities = [i for i in range(num_cities)]
-    unvisited_cities = all_cities.copy()
-    unvisited_cities.remove(start_city)
-    dp = dict()
-    path = dict()
+    distance_cost = coordinate.transfer_coordinate_to_cost_matrix(coordinates)   # 座標轉換成距離矩陣
+    num_cities = len(distance_cost)   # 城市數量
+    all_cities = [i for i in range(num_cities)]   # 建立所有城市的列表
+    unvisited_cities = all_cities.copy()   # 建立所有城市列表的副本
+    unvisited_cities.remove(start_city)   # 移除起點
+    dp = dict()  # 動態規劃字典
+    path = dict()   # 記錄最佳路徑
 
     def tsp(current_city, cities_to_visit):
-        cities_to_visit_t = (current_city, tuple(sorted(cities_to_visit)))
-        if cities_to_visit_t in dp:
-            # print(current_city, cities_to_visit)
+        cities_to_visit_t = (current_city, tuple(sorted(cities_to_visit)))   # 轉換成 tuple
+        if cities_to_visit_t in dp:   # 如果已經計算過，直接回傳 value
             return dp[cities_to_visit_t]
         
-        if not cities_to_visit:
-            cost = distance_cost[current_city][start_city]
-            dp[cities_to_visit_t] = cost
+        if not cities_to_visit:   # 如果所有城市都走過一次，回到起點
+            cost = distance_cost[current_city][start_city]   # 回到起點的距離
+            dp[cities_to_visit_t] = cost    # 更新動態規劃字典
             return cost
         
-        min_cost = float('inf')
-        min_cost_path = None
+        min_cost = float('inf')   # 初始化最小距離成本
+        min_cost_path = None    # 初始化最小距離成本的路徑
         for next_city in cities_to_visit:
-            remaining_cities = cities_to_visit.copy()
-            remaining_cities.remove(next_city)
-            cost = distance_cost[current_city][next_city] + tsp(next_city, remaining_cities)
+            remaining_cities = cities_to_visit.copy()   # 剩餘城市副本
+            remaining_cities.remove(next_city)   # 移除下一個城市
+            cost = distance_cost[current_city][next_city] + tsp(next_city, remaining_cities)    # 計算成本
             
-            if cost < min_cost:
-                min_cost = cost
-                min_cost_path = next_city
+            if cost < min_cost:    # 如果成本小於最小成本
+                min_cost = cost    # 更新最小成本
+                min_cost_path = next_city   # 更新最小成本的路徑
 
-        dp[cities_to_visit_t] = min_cost
-        path[cities_to_visit_t] = min_cost_path
-        # print(current_city, cities_to_visit, min_cost)
+        dp[cities_to_visit_t] = min_cost    # 更新動態規劃字典
+        path[cities_to_visit_t] = min_cost_path    # 更新最佳路徑
         return min_cost
 
     def get_best_path(start_city, unvisited_cities):
-        best_path = [start_city]
-        current_city = start_city
-        cities_to_visit = unvisited_cities.copy()
+        best_path = [start_city]    # 初始化最佳路徑
+        current_city = start_city   # 初始化當前城市
+        cities_to_visit = unvisited_cities.copy()   # 剩餘城市副本
         while cities_to_visit:
-            next_city = path[(current_city, tuple(cities_to_visit))]
-            best_path.append(next_city)
-            cities_to_visit.remove(next_city)
-            current_city = next_city
+            next_city = path[(current_city, tuple(cities_to_visit))]   # 取得下一個城市
+            best_path.append(next_city)   # 加入最佳路徑
+            cities_to_visit.remove(next_city)   # 移除已經走過的城市
+            current_city = next_city   # 更新當前城市
 
-        best_path.append(start_city)
+        best_path.append(start_city)   # 回到起點
         return best_path
     
-    min_cost = tsp(start_city, unvisited_cities)
-    best_path = get_best_path(start_city, unvisited_cities)
+    min_cost = tsp(start_city, unvisited_cities)    # 執行動態規劃
+    best_path = get_best_path(start_city, unvisited_cities)   # 取得最佳路徑
     return min_cost, best_path
